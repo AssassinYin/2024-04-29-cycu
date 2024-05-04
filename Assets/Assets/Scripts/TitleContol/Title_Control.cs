b85d9a2dd9b13ddb3f83a1b0f1113ae5fee7a13d
conarychen <conary0917@gmail.com>
2024-05-04 21:28:38 +0800

    botton測試

---
+++ Assets/Assets/Scripts/TitleContol/Title_Control.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gameStart() {
+        print( "start!!" );
+        //Application.LoadLevel( "" );
+    }
+
+    public void gameSetting() {
+        print( "setting!!" );
+        
+    }
+
+    public void gameLeaving() {
+        print( "Leaving!!" );
+     
+    }
+
+}
